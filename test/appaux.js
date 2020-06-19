54b9a139763b740eb3864c5baefcc70400acf97c
guiarese <guilhermearesende1@gmail.com>
2020-06-18 23:15:58 -0300

    Finalizando Mocha e Supertest

--- test/appaux.js
+++ test/appaux.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const cors= require('cors');
 const requireDir = require('require-dir');
 
 const app = express()
 app.use(express.json())
-app.use(cors());
 require('dotenv/config');
 mongoose.connect(process.env.MONGODB_CONNECTIONSTRING,{useNewUrlParser: true,useUnifiedTopology: true});
-requireDir('./app/models');
-app.use('/api', require('./routes'))
+requireDir('../src/app/models');
+app.use('/api', require('../src/routes'))
 
 app.listen(process.env.PORT || 3000)
 
--- test/hellotest.js
+++
@@ -1,6 +0,0 @@
-var assert = require('assert');
-describe('Basic Mocha String Test', function () {
-  it('should return number of characters is 5', function () {
-    assert.equal("Hello".length, 5);
-  });
-});
